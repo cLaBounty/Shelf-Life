63616e4edc2148a27ceaa0ff1b4141ed453a760a
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-02-25 22:35:42 -0500

    Barebones barcode scanner

--- Shelf-Life-Native/App.js
+++ Shelf-Life-Native/App.js
@@ -1,36 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Cam from './components/barcodeScanner'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('./assets/background.jpg')} style={styles.background}/>
-      <Text style={styles.title}>Shelf Life</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Cam />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  background: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute',
-    opacity: 0.55
-  },
-  title: {
-    fontSize: 50,
-    fontFamily: 'Baskerville-Italic',
-    fontWeight: '300',
-    color: '#fff',
-    letterSpacing: 3
-  }
-});
