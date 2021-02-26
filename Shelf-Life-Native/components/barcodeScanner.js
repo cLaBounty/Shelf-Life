a336e3c42e024710ff7311e5a61f4e36fc70cd10
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-02-26 12:28:17 -0500

    Functional Scanning with Indicator of where to hold item

--- Shelf-Life-Native/components/barcodeScanner.js
+++ Shelf-Life-Native/components/barcodeScanner.js
@@ -1,8 +1,17 @@
+// Code Written by Rhys Sullivan
+
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Animated, TouchableOpacity } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
-function Cam(){
+
+/*
+Helpful Docs: https://docs.expo.io/versions/latest/sdk/bar-code-scanner/
+https://blog.usejournal.com/creating-a-barcode-scanner-using-expo-barcode-scanner-in-a-react-native-cli-project-2d36a235ab41
+
+*/
+
+function Cam() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -24,15 +33,40 @@ function Cam(){
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
   return (
     <View style={styles.container}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
+        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.ean13]}
       />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      <View style={styles.overlay}>
+        <View style={styles.unfocusedContainer}></View>
+        <View style={styles.middleContainer}>
+          <View style={styles.unfocusedContainer}></View>
+          <View
+            style={styles.focusedContainer}>
+            {!scanned && (
+              <Animated.View
+                style={[
+                  styles.animationLineStyle,
+                  {
+                    transform: [
+                      {
+                        translateY: 225
+                      },
+                    ],
+                  },
+                ]}
+              />
+            )}
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}     
+        </View>
+        <View style={styles.unfocusedContainer}></View>
+      </View>
+      <View style={styles.unfocusedContainer}></View>
     </View>
+    </View >
   );
 }
 
@@ -41,6 +75,29 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  unfocusedContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  middleContainer: {
+    flexDirection: 'row',
+    flex: 1.5,
+  },
+  focusedContainer: {
+    flex: 6,
+  },
+  animationLineStyle: {
+    height: 2,
+    width: '100%',
+    backgroundColor: 'red',
   },
 });
 
