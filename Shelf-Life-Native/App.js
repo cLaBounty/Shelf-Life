9ba23dd74f42cefd74440bbf2c0e8a755496b0b2
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-02-27 22:25:28 -0500

    Adjusted app.js to have camera component rendering ability

--- Shelf-Life-Native/App.js
+++ Shelf-Life-Native/App.js
@@ -2,9 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import Cam from './components/barcodeScanner'
-
 export default function App() {
+  if (false){
   return (
-      <Cam />
+    <Cam/>
+    )
+  }
+  return (      
+    <View style={styles.container}>      
+      <ImageBackground source={require('./assets/background.jpg')} style={styles.background}/>
+      <Text style={styles.title}>Shelf Life</Text>
+      <StatusBar style="auto" />      
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+    opacity: 0.55
+  },
+  title: {
+    fontSize: 50,
+    fontFamily: 'Baskerville-Italic',
+    fontWeight: '300',
+    color: '#fff',
+    letterSpacing: 3
+  }
+});
