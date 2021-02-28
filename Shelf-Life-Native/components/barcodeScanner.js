82d30aeb8eae4cbbe91d02b05deae0a2d5747a43
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-02-27 22:20:10 -0500

    Comment pass on the barcode scanner

--- Shelf-Life-Native/components/barcodeScanner.js
+++ Shelf-Life-Native/components/barcodeScanner.js
@@ -1,33 +1,41 @@
 // Code Written by Rhys Sullivan
 
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Animated, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Button, Animated} from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 
 /*
-Helpful Docs: https://docs.expo.io/versions/latest/sdk/bar-code-scanner/
+Helpful Docs: 
+https://docs.expo.io/versions/latest/sdk/bar-code-scanner/
 https://blog.usejournal.com/creating-a-barcode-scanner-using-expo-barcode-scanner-in-a-react-native-cli-project-2d36a235ab41
 
+This is currently a functional component however it may be better suited as a class component
 */
 
 function Cam() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
+
+  // Some reading on 'useEffect'
+  // https://reactjs.org/docs/hooks-effect.html
+  // Important Notes:
+  // Runs on each render & every update
+  // This portion handles camera permissions
+
+  // In this case we are passing an anonymous function to useEffect & calling it right away
+  // https://www.javascripttutorial.net/javascript-anonymous-functions/
+
+  // the [] at the end of UseEffect is to run the effect and clean it up only once, 
+  // this tells react that your effect doesn't depend on any values from props or state,
+  // so it never needs to rerun
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
   }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-
-    // TODO: EXTEND CODE HERE WITH DATA HANDLING
-  };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -36,15 +44,28 @@ function Cam() {
     return <Text>No access to camera</Text>;
   }
 
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+    // TODO: EXTEND CODE HERE WITH DATA HANDLING
+  };
+
   // Can add barCodeTypes={[BarCodeScanner.Constants.BarCodeType.ean13]} to BarCodeScanner
   // for efficiency
 
+  // onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+  // if scanned is true, then we pass undefined in as the function to handle a scanned barcode
+  // this pauses the barcode scanner, allowing for us to handle the scanned result
+
+  // {scanned && ...} is an example of conditional rendering, if scanned is true,
+  // the elements after the && will be added to the screen
+  // https://reactjs.org/docs/conditional-rendering.html
   return (
     <View style={styles.container}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
+        style={StyleSheet.absoluteFillObject}/>
       <View style={styles.overlay}>
         <View style={styles.unfocusedContainer}></View>
         <View style={styles.middleContainer}>
@@ -65,12 +86,12 @@ function Cam() {
                 ]}
               />
             )}
-            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}     
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+          </View>
+          <View style={styles.unfocusedContainer}></View>
         </View>
         <View style={styles.unfocusedContainer}></View>
       </View>
-      <View style={styles.unfocusedContainer}></View>
-    </View>
     </View >
   );
 }
