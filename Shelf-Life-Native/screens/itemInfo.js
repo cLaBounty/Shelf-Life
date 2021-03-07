df03fea6e0bc6de28e61d01f6dbb563523cae28c
cLaBounty <camlabounty@gmail.com>
2021-03-07 02:26:40 -0500

    Added Content to Item Information Screen

--- Shelf-Life-Native/screens/itemInfo.js
+++ Shelf-Life-Native/screens/itemInfo.js
@@ -3,19 +3,53 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default function ItemInfoScreen({ navigation, route }) {
+  const [name, setName] = useState(route.params.itemName);
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+  const [expDate, setExpDate] = useState("");
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <ImageBackground source={require('../assets/background.jpg')} style={styles.background}/>
-
-      {/* Temporary */}
-      <Text style={styles.text}>Item Info Screen</Text>
-      <Text>{route.params.itemName} was scanned...</Text>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.btnText}>Submit</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="Item Name"
+        placeholderTextColor="#9E9791"
+        defaultValue={route.params.itemName}
+        onChangeText={(value) => setName(value)}
+      />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Quantity"
+        placeholderTextColor="#9E9791"
+        keyboardType="numeric"
+        onChangeText={(value) => setQuantity(value)}
+      />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Unit Price"
+        placeholderTextColor="#9E9791"
+        keyboardType="numeric"
+        onChangeText={(value) => setPrice(value)}
+      />
+      <TextInput
+        style={styles.inputField}
+        placeholder="Expiration Date"
+        placeholderTextColor="#9E9791"
+        keyboardType="numbers-and-punctuation"
+        onChangeText={(value) => setExpDate(value)}
+      />
+      <TouchableOpacity style={styles.submitBtn} onPress={() => handleSubmit(name, quantity, price, expDate, navigation)}>
+        <Text style={styles.submitBtnText}>Submit</Text>
       </TouchableOpacity>
     </View>
   );
+}
+
+const handleSubmit = (name, quantity, price, expDate, navigation) => {
+  // TODO: Add item to pantry
+  navigation.navigate('Home');
 }
 
 const styles = StyleSheet.create({
@@ -32,22 +66,26 @@ const styles = StyleSheet.create({
     position: 'absolute',
     opacity: 0.50
   },
-  text: {
-    fontSize: 24,
+  inputField: {
+    width: 300,
+    fontSize: 22,
     color: '#fff',
-    margin: 10
+    margin: 20,
+    padding: 3,
+    paddingLeft: 0,
+    borderColor: '#fff',
+    borderBottomWidth: 1
   },
-  btn: {
-    backgroundColor:'#595959',
+  submitBtn: {
+    backgroundColor:'#5296E7',
     borderColor: '#fff',
     borderWidth: 1,
-    borderRadius: 10,
-    margin: 5
+    borderRadius: 10
   },
-  btnText: {
-    fontSize: 14,
+  submitBtnText: {
+    fontSize: 16,
     color:'#fff',
     padding: 8,
-    letterSpacing: 1.5
+    letterSpacing: 2
   }
 });
