f80e1d7c30fe1f2e85b04cfb90c2c18037ddf01d
Benjamin Roberts <benjamin.roberts01@mymail.champlain.edu>
2021-03-09 23:08:38 -0500

    Overhauled file from functional to class based
    Very little is actually the same here as it was before, though to the user it looks fairly similar. Yay for refactoring to put this file into a much better position moving forward.

--- Shelf-Life-Native/screens/pantry.js
+++ Shelf-Life-Native/screens/pantry.js
@@ -3,63 +3,61 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, ScrollView } from 'react-native';
 import { AlphabetList } from "react-native-section-alphabet-list";
 
+class PantryScreen extends React.Component {
 
-renderHeader = () => {
+  render() {
     return (
-      <View>
-        <Text>header1</Text>
-        <Text>header2</Text>
+      <View style={styles.container}>
+        <StatusBar style="something that causes an error so the status bar is black, thus readable on the white background :)" />
+        <ImageBackground source={require('../assets/background.jpg')} style={styles.background} />
+
+
+        <AlphabetList style={styles.list}
+          data={foodData} // Data for list
+          indexLetterColor={'white'} //Color of letters on right
+
+          renderCustomListHeader={
+            <View>
+              <Text style={styles.listTextInfo}>{"\n\n"}{foodData.length} Pantry Items {"\n"}</Text>
+            </View>
+          }
+
+          renderCustomItem={(item) => (
+            <Text style={styles.listText}>{item.value}</Text>  //Items for list
+          )}
+
+          renderCustomSectionHeader={(section) => (
+            <Text style={styles.seperatorText}>{section.title}</Text> //Seperators
+          )}
+        />
       </View>
-    )
+
+    );
   }
-
-export default function PantryScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="black" />
-      <ImageBackground source={require('../assets/background.jpg')} style={styles.background}/>
-	  
-		  <AlphabetList style={styles.list}
-		  	data = { foodData } // Data for list
-			indexLetterColor = {'white'} //Color of letters on right
-			renderHeader = {this.renderHeader}
-			
-			renderCustomItem = {(item) => (
-				<Text style={styles.listText}>{item.value}</Text>  //Items for list
-			)}
-			
-			renderCustomSectionHeader={(section) => (
-				<Text style={styles.seperatorText}>{section.title}</Text> //Seperators
-			)}
-
-		  />
-    </View>
-		  
-  );
 }
 
 const foodData = [ /* Temp data until the database is connected */
-	{ value: "Jif Chunky Peanutbutter" }, 
-	{ value: "Jello - Raspberry" },
-	{ value: "Jello - Blue Raspberry" },
-	{ value: "Ben's Glorious Crackers" },
-	{ value: "Swedish Fish" },
-	{ value: "Gummy worms" },
-	{ value: "Act II Popcorn" },
-	{ value: "Off-brand poptarts"},
-	{ value: "Little Debbie's brownies" },
-	{ value: "React Native Bits & Bytes"},
-	{ value: "🥺 Zoomer Juice"},
-	{ value: "7up"},
-	{ value: "Tropica Orange Juice"},
-	{ value: "Boomer 💣s"},
-	{ value: "Jif Creamy Peanut Butter"},
-	{ value: "Hood Skim Milk"},
-	{ value: "Oreos Party Sized"},
-	{ value: "Chobani Mixed Berry Yogert"},
-	{ value: "Generic Chicken Noodle Soup"},
-	{ value: "Great Grandma's Awesome Tasting Thanksgiving Roasted Ham"},
-	{ value: "Canada Dry Ginger Ale"},
+  { value: "Jif Chunky Peanutbutter" },
+  { value: "Jello - Raspberry" },
+  { value: "Jello - Blue Raspberry" },
+  { value: "Ben's Glorious Crackers" },
+  { value: "Swedish Fish" },
+  { value: "Gummy worms" },
+  { value: "Act II Popcorn" },
+  { value: "Off-brand poptarts" },
+  { value: "Little Debbie's brownies" },
+  { value: "React Native Bits & Bytes" },
+  { value: "🥺 Zoomer Juice" },
+  { value: "7up" },
+  { value: "Tropica Orange Juice" },
+  { value: "Boomer 💣s" },
+  { value: "Jif Creamy Peanut Butter" },
+  { value: "Hood Skim Milk" },
+  { value: "Oreos Party Sized" },
+  { value: "Chobani Mixed Berry Yogert" },
+  { value: "Generic Chicken Noodle Soup" },
+  { value: "Great Grandma's Awesome Tasting Thanksgiving Roasted Ham" },
+  { value: "Canada Dry Ginger Ale" },
 ]
 
 const styles = StyleSheet.create({
@@ -84,7 +82,7 @@ const styles = StyleSheet.create({
     margin: 10
   },
   btn: {
-    backgroundColor:'#595959',
+    backgroundColor: '#595959',
     borderColor: '#fff',
     borderWidth: 1,
     borderRadius: 10,
@@ -92,36 +90,45 @@ const styles = StyleSheet.create({
   },
   btnText: {
     fontSize: 14,
-    color:'#fff',
+    color: '#fff',
     padding: 8,
     letterSpacing: 1.5
   },
   list: {
-	  width: "95%",
-	  height: "100%",
-	  justifyContent: "center",
-	  backgroundColor:'#59595959',
+    width: "95%",
+    height: "100 %",
+    justifyContent: "center",
+    backgroundColor: '#59595959',
+    paddingTop: 0,
 
   },
   listText: {
-	  width: "100%",
-	  paddingTop: 5,
-	  paddingBottom: 5,
-	  marginTop: 1,
-	  marginBottom: 1,
-      fontSize: 16,
-      color: '#fff',
-	  backgroundColor: "#00000044",
+    width: "100%",
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginTop: 1,
+    marginBottom: 1,
+    fontSize: 16,
+    color: '#fff',
+    backgroundColor: "#00000044",
   },
   seperatorText: {
-	  width: "100%",
-      fontSize: 16,
-      color: '#fff',
-	  fontWeight: "bold",
-	  paddingTop: 5,
-	  paddingBottom: 5,
-	  marginTop: 2,
-	  marginBottom: 2,
-      backgroundColor: "#00000099",
+    width: "100%",
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: "bold",
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginTop: 2,
+    marginBottom: 2,
+    backgroundColor: "#00000099",
+  },
+  listTextInfo: {
+    fontSize: 24,
+    color: "#fff",
+    justifyContent: 'center',
+    textAlign: 'center',
   }
 });
+
+export default PantryScreen;
