9cb1a00ad58121557af01d63fe83f5a132e057d2
Benjamin Roberts <benjamin.roberts01@mymail.champlain.edu>
2021-03-10 23:56:48 -0500

    Updated files that link to itemInfo.js

--- Shelf-Life-Native/screens/scanItem.js
+++ Shelf-Life-Native/screens/scanItem.js
@@ -12,7 +12,7 @@ export default function ScanItemScreen({ navigation }) {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.text}>Scan Item Screen</Text>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Item Information', { itemName: "Peanut Butter" })}>
+      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Item Information', { itemName: "", itemQuantity: "", itemUnitPrice: "", itemExpDate: "" })}>
         <Text style={styles.btnText}>Take Picture</Text>
       </TouchableOpacity>
     </View>
