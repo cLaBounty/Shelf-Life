902f2794a6970907b4514c06b5f1217dfc3d6cc5
Benjamin Roberts <benjamin.roberts01@mymail.champlain.edu>
2021-03-12 01:20:33 -0500

    Initial selection in place
    Plan is to move to @react-native-picker/picker later, but this was used to get the ball rolling since setup is faster.

--- Shelf-Life-Native/screens/recipes.js
+++ Shelf-Life-Native/screens/recipes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { Pages } from 'react-native-pages';
+import ModalDropdown from 'react-native-modal-dropdown';
 
 export default function RecipesScreen({ navigation }) {
   return (
@@ -8,11 +10,40 @@ export default function RecipesScreen({ navigation }) {
       <StatusBar style="auto" />
       <ImageBackground source={require('../assets/background.jpg')} style={styles.background}/>
 
-      {/* Temporary */}
-      <Text style={styles.text}>Recipes Screen</Text>
+
+			<View style={styles.pageStyle}>
+			<Pages>
+        <View>
+					<Text style={styles.header}>Noodles</Text>
+					<ModalDropdown 
+						options={noodleTypes} 
+						defaultIndex={1} 
+						textStyle={styles.type}
+						dropdownStyle={styles.dropdown}
+						dropdownTextStyle={styles.textDropdown}
+						dropdownTextHighlightStyle={styles.textDropdownSelected}
+					/>
+				<Text style={styles.header}>Test</Text>
+				</View>                     
+        <View> 
+					<Text style={styles.header}>Page 2</Text>
+				</View>                     
+        <View> 
+					<Text style={styles.header}>Page 3</Text>
+				</View>
+      </Pages>
+			</View>
     </View>
   );
 }
+
+const noodleTypes = [
+	"Decorative",
+	"Ribbon Cut",
+	"Long",
+	"Short",
+	"Eh"
+]
 
 const styles = StyleSheet.create({
   container: {
@@ -28,22 +59,46 @@ const styles = StyleSheet.create({
     position: 'absolute',
     opacity: 0.50
   },
-  text: {
-    fontSize: 24,
+  header: {
+    fontSize: 50,
     color: '#fff',
-    margin: 10
+		width:"100%",
+		backgroundColor: "#11111166",
+		textAlign: "center",
+		paddingTop: 5,
+		marginTop: 25,
+    overflow: "hidden"
   },
-  btn: {
-    backgroundColor:'#595959',
-    borderColor: '#fff',
-    borderWidth: 1,
-    borderRadius: 10,
-    margin: 5
-  },
-  btnText: {
-    fontSize: 14,
-    color:'#fff',
-    padding: 8,
-    letterSpacing: 1.5
-  }
+	type: {
+		fontSize: 25,
+		color: "#fff",
+		backgroundColor: "#11111166",
+		textAlign: "center",
+		paddingBottom: 5,
+		width: "100%",
+	},
+	pageStyle: {
+		width: "100%",
+		height: "100%",
+	},
+	dropdown:{
+		width:"100%",
+		backgroundColor: "#11111100",
+		paddingTop: 5,
+    overflow: "hidden",
+		borderWidth: 0,
+	},
+	textDropdown: {
+    fontSize: 25,
+		color: "#fff",
+		backgroundColor: "#11111166",
+		textAlign: "center",
+		paddingTop: 5,
+	},
+	textDropdownSelected: {
+    fontSize: 25,
+		color: "#fff",
+		backgroundColor: "#55551166",
+		textAlign: "center",
+	}
 });
--- dependencies.sh
+++ dependencies.sh
@@ -7,5 +7,7 @@ npm install -g expo-cli
 expo install expo-barcode-scanner
 npm install @react-navigation/native @react-navigation/stack
 expo install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+npm install --save react-native-pages
+npm i https://github.com/siemiatj/react-native-modal-dropdown -save
 cd ..
 echo -e "${GREEN}All dependencies were successfully installed"
