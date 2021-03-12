727294d7d0801711a7c1b6ebb9fbc94455df0a47
wkloppenberg <61422251+wkloppenberg@users.noreply.github.com>
2021-03-12 03:30:04 -0500

    Added separate local StyleSheets

--- Shelf-Life-Native/screens/itemInfo.js
+++ Shelf-Life-Native/screens/itemInfo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, TextInput, View, ImageBackground, TouchableOpacity } from 'react-native';
-import styles from '../Style'
+import { StyleSheet, Text, TextInput, View, ImageBackground, TouchableOpacity } from 'react-native';
+import styles from '../Style';
 
 export default function ItemInfoScreen({ navigation, route }) {
   const [name, setName] = useState(route.params.itemName);
@@ -14,35 +14,35 @@ export default function ItemInfoScreen({ navigation, route }) {
       <StatusBar style="auto" />
       <ImageBackground source={require('../assets/background.jpg')} style={styles.background}/>
       <TextInput
-        style={styles.inputField}
+        style={infoStyles.inputField}
         placeholder="Item Name"
         placeholderTextColor="#9E9791"
         defaultValue={route.params.itemName}
         onChangeText={(value) => setName(value)}
       />
       <TextInput
-        style={styles.inputField}
+        style={infoStyles.inputField}
         placeholder="Quantity"
         placeholderTextColor="#9E9791"
         keyboardType="numeric"
         onChangeText={(value) => setQuantity(value)}
       />
       <TextInput
-        style={styles.inputField}
+        style={infoStyles.inputField}
         placeholder="Unit Price"
         placeholderTextColor="#9E9791"
         keyboardType="numeric"
         onChangeText={(value) => setPrice(value)}
       />
       <TextInput
-        style={styles.inputField}
+        style={infoStyles.inputField}
         placeholder="Expiration Date"
         placeholderTextColor="#9E9791"
         keyboardType="numbers-and-punctuation"
         onChangeText={(value) => setExpDate(value)}
       />
-      <TouchableOpacity style={styles.submitBtn} onPress={() => handleSubmit(name, quantity, price, expDate, navigation)}>
-        <Text style={styles.submitBtnText}>Submit</Text>
+      <TouchableOpacity style={infoStyles.submitBtn} onPress={() => handleSubmit(name, quantity, price, expDate, navigation)}>
+        <Text style={infoStyles.submitBtnText}>Submit</Text>
       </TouchableOpacity>
     </View>
   );
@@ -52,3 +52,28 @@ const handleSubmit = (name, quantity, price, expDate, navigation) => {
   // TODO: Add item to pantry
   navigation.navigate('Home');
 }
+
+const infoStyles = StyleSheet.create({
+  inputField: {
+    width: 300,
+    fontSize: 22,
+    color: '#fff',
+    margin: 20,
+    padding: 3,
+    paddingLeft: 0,
+    borderColor: '#fff',
+    borderBottomWidth: 1
+  },
+  submitBtn: {
+    backgroundColor:'#5296E7',
+    borderColor: '#fff',
+    borderWidth: 0,
+    borderRadius: 10
+  },
+  submitBtnText: {
+    fontSize: 16,
+    color:'#fff',
+    padding: 8,
+    letterSpacing: 2
+  },
+});
--- Shelf-Life-Native/screens/login.js
+++ Shelf-Life-Native/screens/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, TextInput, View, ImageBackground, TouchableOpacity } from 'react-native';
-import styles from '../Style'
+import { StyleSheet, Text, TextInput, View, ImageBackground, TouchableOpacity } from 'react-native';
+import styles from '../Style';
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState("");
@@ -10,16 +10,49 @@ export default function LoginScreen({ navigation }) {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <ImageBackground source={require('../assets/background.jpg')} style={styles.background}/>
-      <Text style={styles.title}>Shelf Life</Text>
+      <Text style={loginStyles.title}>Shelf Life</Text>
       <TextInput
-        style={styles.loginInput}
+        style={loginStyles.loginInput}
         placeholder="Username"
         placeholderTextColor="#9E9791"
         onChangeText={(value) => setUsername(value)}
       />
-      <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Home', { username: username })}>
-        <Text style={styles.loginBtnText}>Login</Text>
+      <TouchableOpacity style={loginStyles.loginBtn} onPress={() => navigation.navigate('Home', { username: username })}>
+        <Text style={loginStyles.loginBtnText}>Login</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const loginStyles = StyleSheet.create({
+  title: {
+    fontSize: 72,
+    fontFamily: 'Baskerville-Italic',
+    fontWeight: '300',
+    color: '#fff',
+    letterSpacing: 4
+  },
+  loginInput: {
+    width: 300,
+    fontSize: 22,
+    color: '#fff',
+    margin: 25,
+    padding: 3,
+    paddingLeft: 0,
+    borderColor: '#fff',
+    borderBottomWidth: 1
+  },
+  loginBtn: {
+    backgroundColor:'#5296E7',
+    borderColor: '#fff',
+    borderWidth: 0,
+    borderRadius: 10
+  },
+  loginBtnText: {
+    fontSize: 16,
+    color:'#fff',
+    textTransform: 'uppercase',
+    padding: 8,
+    letterSpacing: 2
+  },
+});
