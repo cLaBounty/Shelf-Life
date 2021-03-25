a459d1b3ac76f921505355d973f7f0bd4905d842
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2021-03-25 12:54:26 -0400

    Updated Server Code With Comments

--- Server/server.py
+++ Server/server.py
@@ -11,76 +11,139 @@ import urllib.parse
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1> hello </h1>"
 
+
 @app.route('/api/barcode/', methods=['GET'])
-def barcode():    
-    # testing code - 737628064502
+def barcode():
+    """
+        Handles getting the official name from the barcode API
+
+        INPUT:
+        Expects the barcode in the url as an argument, i.e 
+        /api/barcode/?barcode=NNN
+
+        
+        OUTPUT:
+        {
+            "Status": "OK"                        
+            "Official Name": "Rossco Thin Spaghetti"
+        }
+
+    Sample Curl:
+    curl http://127.0.0.1:5000/api/barcode/?barcode=737628064502
+    """
     params = request.args
     api_response_json = None
-    if 'barcode' in params:        
-        barcode = params['barcode']        
-        
-        api_response = requests.get('https://world.openfoodfacts.org/api/v0/product/{0}.json'.format(barcode))
+    response_dict = {}
+    response_dict["Status"] = "ERROR"
+    if 'barcode' in params:
+        barcode = params['barcode']
+
+        api_response = requests.get(
+            'https://world.openfoodfacts.org/api/v0/product/{0}.json'.format(barcode))
         api_response_json = api_response.json()
-        if api_response_json['status_verbose'] == 'product found':
-            item_official_name = api_response_json["product"]["product_name_en"]
-
-            
         
-            # TODO: Remove any non ABC chars, (include whitespace)
-            split_product_name = item_official_name.split(' ')                
-            response = "NEED_SELECTION^"
-            for x in range(len(split_product_name)-1):
-                response += split_product_name[x] + '|'
-            response += split_product_name[len(split_product_name)-1] 
-            return api_response_json
-        else:
-            response = "ERROR^Product Not Found"                                            
-            return response
+        if api_response_json['status_verbose'] == 'product found':
+            response_dict['Status'] = "OK"            
+            response_dict["Official Name"] = api_response_json["product"]["product_name_en"]
+            return response_dict        
+    return response_dict
 
-    return "error"
 
-@app.route('/api/selection/', methods=['GET'])
-def selection():    
-    params = request.args    
-    if 'selection' in params:        
-        data = params[selection]
-        official_name = "pizza"
-        category = "MISSING"
-        common_name = "MISSING"
+@app.route('/api/selection/', methods=['GET', 'POST'])
+def selection():
+    """
+        Handles converting the selected parts of a scanned item official name into their common ingredient names.
 
-        data = findCommonNameFromOfficial(data.split('|'))
-        category = data[0]
-        common_name = data[1]
+        INPUT:
+        Expects a JSON object in the request data that is in this format:
+        {
+            "Selection": [
+                "Spaghetti",                
+            ],
+            "Official Name": "Rossco Thin Spaghetti"
+        }
 
-        response = 'ALL_INFO^' + official_name + '|' + category + '|' + common_name                
-        return response
-    return "error"
+        
+        OUTPUT:
+        {            
+            "Category": "Long Noodles",
+            "Common Name": "spaghetti",
+            "Official Name": "Rossco Thin Spaghetti"
+        }
+
+
+        Sample curl tester:
+        curl -d '{"Selection": ["spaghetti"], "Official Name": "Rossco Thin Spaghetti"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/api/selection/
+    """
+    info_dict = request.json
+
+    user_selection = info_dict["Selection"]
+    data = findCommonNameFromOfficial(user_selection)
+
+    response_dict = {}
+    response_dict['Official Name'] = info_dict["Official Name"]
+    response_dict['Category'] = data[0]
+    response_dict['Common Name'] = data[1]
+    return response_dict
+
 
 @app.route('/api/user/', methods=['GET'])
-def userinfo():               
-    response = dbConnector.getUserInformation()    
+def userinfo():
+    """    
+        Purely a testing function, outputs all of the user information to a webpage
+    """
+    response = dbConnector.getUserInformation()
     outstr = ""
     for entry in response:
-        outstr += str(entry)    
+        outstr += str(entry)
     return outstr
+
 
 @app.route('/api/user/new', methods=['GET', 'POST'])
 def makeNewUser():
-    """            
-        curl -d '{"username":"Rhys Sullivan", "password":"passowrd", "display_name":"rhysS"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/api/user/new 
+    """    
+        Handles adding a new user to the database
+
+        INPUT:
+        Expects a JSON object in the request data that is in this format:
+        {
+            "password": "password",
+            "email": "email@email.com",
+            "display_name" "Display Name"
+        }
+
+        
+        OUTPUT:
+        {            
+            "Status": "OK",                    
+        }
+
+
+        TODO:
+        -Hash Passwords Before Sending
+        -Email Verification
+        -Handle different cases in the response i.e
+            -Invalid Password
+            -Invalid Email
+        
+        Sample curl tester:                        
+        curl -d '{"email":"rhys@rhyssullivan.com", "password":"passowrd", "display_name":"rhysS"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/api/user/new 
     """
-            
+
     info_dict = request.json
-    print(info_dict)    
     password = info_dict['password']
-    username = info_dict['username']
+    username = info_dict['email']
     display_name = info_dict['display_name']
     dbConnector.addNewUser(username, password, display_name)
-    return info_dict
+    
+    response_dict = {}
+    response_dict["Status"] = "OK"
+    return response_dict
+
 
 app.run()
-print("Closed Server")
