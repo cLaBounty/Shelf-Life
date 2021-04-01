6678025881ae5a45f2753f1064d8333aef04a390
Benjamin Roberts <benjamin.roberts01@mymail.champlain.edu>
2021-04-01 01:44:12 -0400

    Initial move to tabs
    Basically all other navigation is currently broken, as well as a lot of the UI stuff. This patch just adds in the tabs at the bottom. More to come ;)

--- Shelf-Life-Native/App.js
+++ Shelf-Life-Native/App.js
@@ -23,17 +23,6 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-
-        <Stack.Screen name="Add Item" component={AddItemScreen} options={{ headerShown: true }} />
-        <Stack.Screen name="Scan Item" component={ScanItemScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Item Information" component={ItemInfoScreen} options={{ headerShown: false }} />
-
-        <Stack.Screen name="Pantry" component={PantryScreen} options={{ headerShown: true }} />
-        <Stack.Screen name="Recipes" component={RecipesScreen} options={{ headerShown: true }} />
-        <Stack.Screen name="Recipe Information" component={RecipeInfoScreen} options={{ headerShown: true }} />				
-        <Stack.Screen name="Nutrition" component={NutritionScreen} options={{ headerShown: true }} />
-        <Stack.Screen name="Expenses" component={ExpensesScreen} options={{ headerShown: true }} />
-        <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: true }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Shelf-Life-Native/screens/home.js
+++ Shelf-Life-Native/screens/home.js
@@ -1,53 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import styles from '../Style';
 
+import PantryScreen from './pantry';
+import RecipesScreen from './recipes';
+import NutritionScreen from './nutrition';
+import ExpensesScreen from './expenses';
+import SettingsScreen from './settings';
+
 export default function HomeScreen({ navigation, route }) {
+	const [username, setUsername] = useState(route.params.username);
+	const Tab = createBottomTabNavigator();
+	
 	return (
-		<View style={styles.container}>
-			<StatusBar style="auto" />
-			<ImageBackground source={require('../assets/background.jpg')} style={styles.background}/>
-			<Text style={styles.text}>Hi, {route.params.name}!</Text>
-			<View style={homeStyles.container}>
-		  		<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Add Item')}>
-					<Image style={styles.btnImage} source={require('../assets/additem.png')}/>
-					<Text style={styles.btnText}>Add Item</Text>
-		  		</TouchableOpacity>
-		  		<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Pantry')}>
-					<Image style={styles.btnImage} source={require('../assets/pantry.png')}/>
-					<Text style={styles.btnText}>Pantry</Text>
-		  		</TouchableOpacity>
-	  		</View>
-	  		<View style={homeStyles.container}>
-		  		<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Recipes')}>
-				  	<Image style={styles.btnImage} source={require('../assets/recipes.png')}/>
-					<Text style={styles.btnText}>Recipes</Text>
-		  		</TouchableOpacity>
-		  		<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Nutrition')}>
-				  <Image style={styles.btnImage} source={require('../assets/nutrition.png')}/>
-					<Text style={styles.btnText}>Nutrition</Text>
-		  		</TouchableOpacity>
-	  		</View>
-	  		<View style={homeStyles.container}>
-		  		<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Expenses')}>
-				  	<Image style={styles.btnImage} source={require('../assets/expenses.png')}/>
-					<Text style={styles.btnText}>Expenses</Text>
-		  		</TouchableOpacity>
-		  		<TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Settings', { username: route.params.username })}>
-				  	<Image style={styles.btnImage} source={require('../assets/settings.png')}/>
-					<Text style={styles.btnText}>Settings</Text>
-		  		</TouchableOpacity>
-	  		</View>
-		</View>
+    	<NavigationContainer independent = {true}>
+			<Tab.Navigator
+			tabBarOptions={{
+				activeTintColor: activeTintColor,
+				inactiveTintColor: inactiveTintColor,
+			}}
+			>
+
+				<Tab.Screen name="Pantry" component={PantryScreen}
+				options={{
+					tabBarIcon: () => {
+						return (
+							<Image style={homeStyles.tabIcons} source={require('../assets/pantry.png')} />
+					)}}}
+				/>
+
+				<Tab.Screen name="Recipes" component={RecipesScreen}
+				options={{
+					tabBarIcon: () => {
+						return (
+							<Image style={homeStyles.tabIcons} source={require('../assets/recipes.png')} />
+					)}}}
+				/>
+
+				<Tab.Screen name="Nutrition" component={NutritionScreen}
+				options={{
+					tabBarIcon: () => {
+					    return (
+							<Image style={homeStyles.tabIcons} source={require('../assets/nutrition.png')} />
+					)}}}
+				/>
+
+				<Tab.Screen name="Expenses" component={ExpensesScreen}
+				options={{
+					tabBarIcon: () => {
+					    return (
+							<Image style={homeStyles.tabIcons} source={require('../assets/expenses.png')} />
+					)}}}
+				/>
+
+				<Tab.Screen name={`Settings`} component={SettingsScreen} 
+			 	listeners={({ navigation, route }) => ({
+	                tabPress: e => {
+	                	navigation.navigate('Settings', {username: username})
+					}})} 
+				options={{
+					tabBarIcon: (focused) => {
+						return (
+							<Image style={[homeStyles.tabIcons]} source={require('../assets/settings.png')} />
+						)
+					}}}
+				/>
+
+     	 	</Tab.Navigator>
+		</NavigationContainer>
 	);
 }
 
+
+const activeTintColor = "#ff4444"
+const inactiveTintColor = "#888"
+const iconSize = 40;
+const tint = "#888"
+
 const homeStyles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}
-});
+	tabIcons: {
+		width: iconSize,
+		height: iconSize,
+		tintColor: activeTintColor
+	},
+	active: {
+		tintColor: activeTintColor
+	},
+	inactive: {
+		tintColor: inactiveTintColor
+	},
+})
