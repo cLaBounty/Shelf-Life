628b752fb551a4b0c1cfdc7b34cdbe31554ea98f
Benjamin Roberts <benjamin.roberts01@mymail.champlain.edu>
2021-04-06 01:30:23 -0400

    Added base for a search, style updates, and android binary update

--- Shelf-Life-Native/screens/pantry.js
+++ Shelf-Life-Native/screens/pantry.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, SafeAreaView } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import { AlphabetList } from "react-native-section-alphabet-list";
 import { FloatingAction } from "react-native-floating-action";
 import styles from '../Style';
@@ -9,60 +10,81 @@ const GLOBAL = require('../Globals')
 const pantryJSON = require('../assets/pantryTest.json')
 
 export default function PantryScreen({ navigation }) {
-  return (
-	  <SafeAreaView style={styles.safeArea}>
-	      <View style={styles.container}>
-			  <ImageBackground source={require('../assets/background.jpg')} style={styles.background} />
-		      <AlphabetList style={pantryStyles.list}
-		  		 data={getPantry()}
-		        indexLetterColor={'white'} //Color of letters on right
 
-		  			renderCustomItem={(item) => ( //Make the data fancy lookin'
-		  				formatPantry( item, {navigation} )
-		        )}
+	const [searchQ, setSearchQ] = useState("")
 
-		        renderCustomSectionHeader={(section) => ( //Seperators
-		          <Text style={pantryStyles.seperatorText}>{section.title}</Text>
-		        )}
-		      />
-	      </View>
-		  <FloatingAction
-		  actions={addActions}
-		  color={GLOBAL.ACCENT_COLOR}
-		  iconHeight = {22}
-		  iconWidth = {22}
-		  overlayColor={"rgba(0,0,0,0)"}
-		  icon={require('../assets/settings.png')}
-		  shadow={{ shadowOpacity: 0.3, shadowOffset: { width: 0, height: 0 }, shadowColor: "#000000", shadowRadius: 10 }}
-		  onPressItem={name => {
-		  	if (name == "manual")
-		  	{
-		  		navigation.navigate('Item Info', { itemName: "", itemQuantity: "", itemUnitPrice: "", itemExpDate: "" })
-		  	}
-		  	else if (name == "scan") {
-		  		navigation.navigate('Scan Item')
-		  	}
-		}}/>
-	  </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={styles.safeArea}>
+			<View style={styles.container}>
+				<ImageBackground source={require('../assets/background.jpg')} style={styles.background} />
+				<StatusBar style="sfdsfd" />
+				<View style={pantryStyles.searchView}>
+					<Text style={pantryStyles.header}>Pantry</Text>
+					<SearchBar
+						placeholder="Search"
+						onChangeText={updateSearch}
+						value={searchQ}
+						containerStyle={pantryStyles.searchBar}
+						inputContainerStyle={{height: 30}}
+						platform={"ios"}
+					/>
+				</View>
+
+				<AlphabetList style={pantryStyles.list}
+					data={getPantry()}
+					indexLetterColor={'white'} //Color of letters on right
+
+					renderCustomItem={(item) => ( //Make the data fancy lookin'
+						formatPantry(item, { navigation })
+					)}
+
+					renderCustomSectionHeader={(section) => ( //Seperators
+						<Text style={pantryStyles.seperatorText}>{section.title}</Text>
+					)}
+				/>
+			</View>
+			<FloatingAction
+				actions={addActions}
+				color={GLOBAL.ACCENT_COLOR}
+				iconHeight={22}
+				iconWidth={22}
+				overlayColor={"rgba(0,0,0,0)"}
+				icon={require('../assets/settings.png')}
+				shadow={{ shadowOpacity: 0.3, shadowOffset: { width: 0, height: 0 }, shadowColor: "#000000", shadowRadius: 10 }}
+				onPressItem={name => {
+					if (name == "manual") {
+						navigation.navigate('Item Info', { itemName: "", itemQuantity: "", itemUnitPrice: "", itemExpDate: "" })
+					}
+					else if (name == "scan") {
+						navigation.navigate('Scan Item')
+					}
+				}} />
+		</SafeAreaView>
+	);
+
+	function updateSearch(search) {
+		setSearchQ(search)
+	}
 }
 
 function getPantry() {
 	return pantryJSON.items.map(data => {
 		return (
-			{ dispName: data.dispName, quantity: data.quantity, expDate: data.expDate, price: data.price, value: data.name, key: data.dispName }
+			{ dispName: data.dispName, quantity: data.quantity, expDate: data.expDate, price: data.price, value: data.dispName, key: data.name }
 		)
 	})
 }
 
-function formatPantry(item, { navigation } ){
-		return (
-			<View key = {item.name}>
-				<TouchableOpacity onPress={() => navigation.navigate('Item Info', { itemName: item.dispName, itemQuantity: item.quantity, itemUnitPrice: item.price, itemExpDate: item.expDate })}>
+function formatPantry(item, { navigation }) {
+	return (
+		<View key={item.name}>
+			<TouchableOpacity onPress={() => navigation.navigate('Item Info', { itemName: item.dispName, itemQuantity: item.quantity, itemUnitPrice: item.price, itemExpDate: item.expDate })}>
+				<View style={pantryStyles.listTextView}>
 					<Text style={pantryStyles.listText}>{item.dispName}</Text>
+				</View>
 			</TouchableOpacity>
-			</View>
-		)
+		</View>
+	)
 }
 
 
@@ -84,42 +106,58 @@ const addActions = [
 ]
 
 const pantryStyles = StyleSheet.create({
-  test: {
-	width: "100%",
-  },
-  list: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    backgroundColor: '#59595959',
-    paddingTop: 0,
+	searchView: {
+		paddingTop: 70,
+		alignItems: "center",
+		width: "100%",
+		backgroundColor: "#fff",
+	},
+	header: {
+		fontSize: 16,
+		fontWeight: "600",
+	},
+	searchBar: {
+		width: "100%",
+		maxWidth: 400,
+	},
+	list: {
+		width: "100%",
+		height: "100%",
+		justifyContent: "center",
+		backgroundColor: '#59595959',
+		paddingTop: 0,
 
-  },
-  listText: {
-    width: "100%",
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginRight: 10,
-    fontSize: 16,
-    color: '#fff',
-    backgroundColor: "#00000044",
-    paddingLeft: 10,
-  },
-  seperatorText: {
-    width: "100%",
-    fontSize: 16,
-    color: '#fff',
-    fontWeight: "bold",
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingLeft: 10,
-    marginRight: 10,
-    backgroundColor: "#555555ff",
-  },
-  listTextInfo: {
-    fontSize: 24,
-    color: "#fff",
-    justifyContent: 'center',
-    textAlign: 'center',
-  }
+	},
+	listTextView: {
+		borderColor: "#000",
+		borderTopWidth: 1,
+		borderBottomWidth: 1
+	},
+	listText: {
+		width: "100%",
+		paddingTop: 10,
+		paddingBottom: 10,
+		marginRight: 10,
+		fontSize: 16,
+		color: '#000',
+		backgroundColor: "#eee",
+		paddingLeft: 10,
+	},
+	seperatorText: {
+		width: "100%",
+		fontSize: 16,
+		color: '#000',
+		fontWeight: "bold",
+		paddingTop: 5,
+		paddingBottom: 5,
+		paddingLeft: 10,
+		marginRight: 10,
+		backgroundColor: "#bbbf",
+	},
+	listTextInfo: {
+		fontSize: 24,
+		color: "#fff",
+		justifyContent: 'center',
+		textAlign: 'center',
+	}
 });
